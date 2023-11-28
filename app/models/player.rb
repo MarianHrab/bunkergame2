fe1618b2509a327641383b6beccdb693b30e5707
MarianHrab <marian.l.hrab@ukd.edu.ua>
2023-11-28 13:40:33 +0200

    added lock icons

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,6 @@
 class Player < ApplicationRecord
   serialize :opened_characteristics, Array, coder: JSON
-  serialize :visible_characteristics, Array
+  serialize :visible_characteristics, Array, coder: JSON
   belongs_to :user
   belongs_to :room
   has_one :characteristic, dependent: :destroy
